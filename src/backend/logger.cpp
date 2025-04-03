906e77e8bc12cebb8a2de72e9f7704b2665e917d
Lukáš Pšeja <luk4sps3j4@gmail.com>
2025-04-03 11:49:40 +0200

    custom qt logging messages implementation

--- src/backend/logger.cpp
+++ src/backend/logger.cpp
@@ -1,41 +1,36 @@
+/**
+ * @file logger.cpp
+ * @brief Implements the Logger class for logging with colored output.
+ *
+ * This file contains the implementation of the Logger class, including the
+ * custom message handler for Qt logging. The message handler formats log
+ * messages with appropriate colors and prefixes based on the log level
+ * (e.g., Debug, Info, Warning, Critical).
+ *
+ * @author Lukas Pseja (xpsejal00)
+ * @date 03-04-2025
+ */
 #include "logger.hpp"
 
-void Logger::logError(const char *fmt, ...)
+void Logger::messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &message)
 {
-    va_list args;
-    va_start(args, fmt);
+    QString formattedMessage;
 
-    QString message = formatMessage(fmt, args);
-    fprintf(stderr, "%s[Error] %s%s\n", TerminalColors::RED, TerminalColors::RES, message.toUtf8().constData());
+    switch (type)
+    {
+    case QtDebugMsg:
+        formattedMessage = QString("%1[Debug]%2 %3").arg(TerminalColors::BLU, TerminalColors::RES, message);
+        break;
+    case QtInfoMsg:
+        formattedMessage = QString("%1[Info]%2 %3").arg(TerminalColors::CYN, TerminalColors::RES, message);
+        break;
+    case QtWarningMsg:
+        formattedMessage = QString("%1[Warning]%2 %3").arg(TerminalColors::YEL, TerminalColors::RES, message);
+        break;
+    case QtCriticalMsg:
+        formattedMessage = QString("%1[Error]%2 %3").arg(TerminalColors::RED, TerminalColors::RES, message);
+        break;
+    }
 
-    va_end(args);
-}
-
-void Logger::logWarning(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-
-    QString message = formatMessage(fmt, args);
-    fprintf(stderr, "%s[Warning] %s%s\n", TerminalColors::YEL, TerminalColors::RES, message.toUtf8().constData());
-
-    va_end(args);
-}
-
-void Logger::logInfo(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-
-    QString message = formatMessage(fmt, args);
-    fprintf(stderr, "%s[Info] %s%s\n", TerminalColors::CYN, TerminalColors::RES, message.toUtf8().constData());
-
-    va_end(args);
-}
-
-QString Logger::formatMessage(const char *fmt, va_list args)
-{
-    char buffer[1024];
-    vsnprintf(buffer, sizeof(buffer), fmt, args);
-    return QString(buffer);
+    fprintf(stdout, "%s\n", formattedMessage.toUtf8().constData());
 }
