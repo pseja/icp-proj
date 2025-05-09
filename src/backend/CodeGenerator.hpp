33f1826d57a96bee894f620c3fafdcd90e82c218
Matúš Csirik <xcsirim00@fit.vutbr.cz>
2025-05-09 12:51:28 +0200

    formatting changes and raw string usage

--- src/backend/CodeGenerator.hpp
+++ src/backend/CodeGenerator.hpp
@@ -20,39 +20,37 @@ class Variable;
  * @brief The CodeGenerator class generates C++ code from FSM instances
  */
 class CodeGenerator : public QObject {
-  Q_OBJECT
+    Q_OBJECT
 
- public:
-  explicit CodeGenerator(QObject *parent = nullptr);
+   public:
+    explicit CodeGenerator(QObject *parent = nullptr);
 
-  /**
-   * @brief Generates C++ code from a state machine
-   * @param fsm The state machine to generate code from
-   * @return Generated C++ code
-   */
-  QString generateCode(FSM *fsm);
+    /**
+     * @brief Generates C++ code from a state machine
+     * @param fsm The state machine to generate code from
+     * @return Generated C++ code
+     */
+    QString generateCode(FSM *fsm);
 
- private:
-  QString generateHeaders();
-  QString generateVariableDeclarations(FSM *fsm);
-  QString generateRuntimeMonitoring();
-  QString generateHelperFunctions(FSM *fsm);
-  QString generateMainFunction(FSM *fsm);
-  QString generateQStateMachineMain(FSM *fsm);
-  QString generateTcpXmlProtocolServer(FSM *fsm);
-  QString generateStateSetup(FSM *fsm);
-  QString generateTerminalInputHandler(FSM *fsm);
-  QString generateInputEventClass();
-  QString generateGeneratedTransitionClass();
+   private:
+    QString generateHeaders();
+    QString generateVariableDeclarations(FSM *fsm);
+    QString generateRuntimeMonitoring();
+    QString generateHelperFunctions(FSM *fsm);
+    QString generateMainFunction(FSM *fsm);
+    QString generateQStateMachineMain(FSM *fsm);
+    QString generateTcpXmlProtocolServer(FSM *fsm);
+    QString generateStateSetup(FSM *fsm);
+    QString generateTerminalInputHandler(FSM *fsm);
+    QString generateInputEventClass();
+    QString generateGeneratedTransitionClass();
 
-  /**
-   * @brief Generate transition code based on a transition
-   * @param transition The transition to generate code for
-   * @param sourceState The source state
-   * @param targetState The target state
-   * @return Generated C++ code for the transition
-   */
-  QString generateTransitionCode(Transition *transition,
-                                 const State *sourceState,
-                                 const State *targetState);
+    /**
+     * @brief Generate transition code based on a transition
+     * @param transition The transition to generate code for
+     * @param sourceState The source state
+     * @param targetState The target state
+     * @return Generated C++ code for the transition
+     */
+    QString generateTransitionCode(Transition *transition, const State *sourceState, const State *targetState);
 };
--- src/backend/GuiClient.hpp
+++ src/backend/GuiClient.hpp
@@ -12,25 +12,25 @@
 #include <QTcpSocket>
 
 class GuiClient : public QObject {
-  Q_OBJECT
- public:
-  explicit GuiClient(const QString& host = "127.0.0.1", quint16 port = 54323, QObject* parent = nullptr);
-  void connectToServer();
-  void sendCommand(const QString& xml);
-  void sendSet(const QString& name, const QString& value);
-  void sendCall(const QString& name);
-  void sendStatus();
-  void sendHelp();
-  void sendReqFSM();
-  void sendDisconnect();
-  void sendShutdown();
-  void sendPong();
+    Q_OBJECT
+   public:
+    explicit GuiClient(const QString& host = "127.0.0.1", quint16 port = 54323, QObject* parent = nullptr);
+    void connectToServer();
+    void sendCommand(const QString& xml);
+    void sendSet(const QString& name, const QString& value);
+    void sendCall(const QString& name);
+    void sendStatus();
+    void sendHelp();
+    void sendReqFSM();
+    void sendDisconnect();
+    void sendShutdown();
+    void sendPong();
 
- public slots:
-  void onReadyRead();
+   public slots:
+    void onReadyRead();
 
- private:
-  QTcpSocket* socket;
-  QString m_host;
-  quint16 m_port;
+   private:
+    QTcpSocket* socket;
+    QString m_host;
+    quint16 m_port;
 };
